1badfe2620840d16ec68ff5c8777face77812b2a
Akbar Wijaya <151990608+akbarwjyy@users.noreply.github.com>
2025-06-25 00:11:47 +0700

    update styling footer

--- layout/footer.php
+++ layout/footer.php
@@ -3,8 +3,8 @@
 ?>
 </div>
 </div>
-<div class="footer">
-    <p>&copy; <?php echo date("Y"); ?> Industri Rumah Tangga Ampyang Cap Garuda. All rights reserved.</p>
+<div class="footer bg-gray-900 text-white py-4 text-center shadow-inner mt-8 w-full">
+    <p class="text-sm tracking-wide">&copy; <?php echo date("Y"); ?> Industri Rumah Tangga Ampyang Cap Garuda. All rights reserved.</p>
 </div>
 
 <script src="../assets/js/script.js"></script>
